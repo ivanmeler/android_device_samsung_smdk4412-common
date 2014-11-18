11544e8d012baede275129ff8deb47df3608d33a
Dheeraj CVR <cvr.dheeraj@gmail.com>
2014-11-18 16:35:43 +0530

    smdk4412-common: update mediacodecs for lollipop
    Change-Id: I49bcf775aab3a25483c586fea95f4ff6b4c34156

--- common.mk
+++ common.mk
@@ -108,6 +108,9 @@ PRODUCT_PACKAGES += \
 #   libOMX.SEC.VP8.Decoder
 
 PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(COMMON_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     $(COMMON_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
 
