5d9c84ab80c0409489969232917f9730ed3a37e3
RGIB <gibellini.roberto@gmail.com>
2016-05-03 15:20:59 +0200

    smdk4412 : add Gello browser
    Change-Id: I588c1ebff9fe88c91596a27a87d84393641c6edb

--- common.mk
+++ common.mk
@@ -74,6 +74,7 @@ PRODUCT_PACKAGES += \
     audio.r_submix.default \
     audio.usb.default \
     com.android.future.usb.accessory \
+    Gello \
     gralloc.exynos4 \
     hwcomposer.exynos4 \
     libExynosHWCService \
